fa2ba884c1810beecc331b3f7f6f251ae1e7843e
Slavko_V <slavko.vuletic92@gmail.com>
2021-02-07 04:16:22 +0100

    init point

---
+++ SOURCE/app/app.js
@@ -0,0 +1,27 @@
+//╔═══════════════════════════════════════════════════════════════════════════════╗
+//║ ♠ File Name: app.js                                                           ║
+//║ ↔ Location: <: public_root :>/ app.js                                         ║
+//║ Σ Description:                                                                ║
+//║     → In case this lives more than few days, better have something prepared   ║
+//╟       than few days, better have something prepared have as   ╔═══════════════╣
+//║       than few days, better have something prepared.          ║ √ 23.01.2021. ║
+//╚═══════════════════════════════════════════════════════════════╩═══════════════╝
+
+
+(function () {
+    console.log('App STARTING file..>.>.>.>')
+
+    if (tru_org !== window.location.origin) {
+        console.log('Origin not cool. Mkey? ')
+        window.location.replace(tru_org);
+    }
+
+
+})()
+
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+//▌   ! END OF FILE !            ▐▀▀▀         ▐▀▀▀             ! END OF FILE !    ▐
+//▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄      ▐▀▀▀  ▐▀▀▀▀▌ ▐▀▀▀         ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐
+//▌ ♠ File Name: app.js   ▐      ▐▄▄▄  ▐▄▄▄▄▌ ▐            ▌  ☺ Author: Slavko V. ▐
+//▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
